53b64159d89934d07504fa5037a061e32e6a61bf
23662032-beep <23662032@kthcm.edu.vn>
2025-10-30 08:09:51 +0700

    Add files via upload

---
+++ thanh/src/App.tsx
@@ -0,0 +1,36 @@
+import "./styles.css";
+// @ts-ignore
+import Home from "./Home";
+// @ts-ignore
+import Layout from "./Layout";
+// @ts-ignore
+import Trang1 from "./Trang1";
+// @ts-ignore
+import Chitietsanpham from "./Chitietsanpham";
+// @ts-ignore
+import Trang2 from "./Trang2";
+// @ts-ignore
+import ListProducts from "./ListProducts";
+
+// @ts-ignore
+import ListProducts_SP from "./ListProducts_SP";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  //return <Layout />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<ListProducts_SP />} />
+          <Route path="trang1" element={<Trang1 />} />
+          <Route path="sanpham/:id" element={<Chitietsanpham />} />
+          <Route path="trang2" element={<Trang2 />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
